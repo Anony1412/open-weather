bd4ffcb552b0dff1f18c34786d1302f8c732908b
Anony1412 <nqbao.ptit@gmail.com>
2020-10-07 14:51:17 +0700

    task_1.1: Apply project structure follow MVP Pattern

---
+++ lib/ui/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(home: SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold();
+}
